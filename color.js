0e1f1e072ee8656ffaafa616582971d2f41277e0
Dhanushvarma A V <130209421+Dhanushvarma1552@users.noreply.github.com>
2023-09-17 21:44:57 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,14 @@
+let out=document.getElementById("colo")
+let b=document.getElementsByTagName("body")[0]
+function change(){
+    let arr=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    let clr='#'
+    let a;
+    for(let i=0;i<6;i++){
+    a=Math.floor(Math.random()*16)
+    clr+=arr[a]
+    }
+    console.log(clr)
+    out.innerHTML=clr
+    b.style.backgroundColor=clr
+}
